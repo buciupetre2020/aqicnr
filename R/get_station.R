27e8720810cb22b1a4ada453913249e3646933a5
buciupetre2020 <buciu_petre2013@yahoo.com>
2021-04-08 17:57:13 +0300

    Initial commit

---
+++ R/get_station.R
@@ -0,0 +1,26 @@
+#' Get the air quality and air pollution data from a station
+#'
+#' @param id The ID of the station (Run stations() function to get these)
+#'
+#' @return A tibble with all the measurements available.
+#' @export
+#'
+#' @examples
+#' get_station("125707")
+#'
+get_station <- function(id){
+  message(paste("Processing id", id, "..."))
+  link <- paste0("https://airnet.waqi.info/airnet/feed/hourly-cached/", id)
+  ceva <- httr::content(httr::GET(link))
+  if(ceva$status!="ok"){
+    return(NA)
+  } else {
+    indicatori <- names(ceva$data)
+    rez <- dplyr::tibble(ceva=ceva$data) %>%
+      dplyr::mutate(indicator = indicatori) %>%
+      tidyr::unnest(ceva) %>%
+      tidyr::unnest_wider(ceva) %>%
+      dplyr::mutate(id = id) %>% dplyr::relocate(indicator)
+  }
+return(rez)
+}
---
+++ R/stations.R
@@ -0,0 +1,38 @@
+edit_station <- function(x){
+  lat = x$g[[1]]
+  lon = x$g[[2]]
+  address = x$n
+  id = x$x
+  u = x$u
+  c = x$c
+  a = x$a
+  S = x$S
+  return(data.frame("address"=address, "id"=id, "lat"=lat,
+                    "lon"=lon, "u"=u, "c"=c, "a"=a,"S"=S))
+}
+
+edit_station <- purrr::possibly(edit_station,
+                                data.frame("address"=NA, "id"=NA, "lat"=NA,
+                                           "lon"=NA, "u"=NA, "c"=NA, "a"=NA, "S"=NA))
+
+
+#' Get all the stations in your chosen area.
+#'
+#' @param llon Longitude of the left boundary
+#' @param rlon Longitude of the right boundary
+#' @param ulat Latitude of the upper boundary
+#' @param dlat Latitude of the lower boundary
+#'
+#' @return Returns a data.frame with basic informations about the stations.
+#' @export
+#'
+#' @examples
+#' stations(llon=25.93, rlon=26.2, ulat=44.64, dlat=44.30)
+#'
+stations <- function(llon, rlon, ulat, dlat){
+  bounds <- paste(llon, dlat, rlon, ulat, sep=",")
+  statii <- httr::content(httr::POST("https://airnet.waqi.info/airnet/map/bounds",
+                                     body = list(bounds=bounds),
+                                     encode = "multipart"))$data
+  return(purrr::map_dfr(statii, edit_station))
+}
